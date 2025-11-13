c8615a4b1e758aab4ece17f69336ccbff4bd0dda
KReshma333 <reshmakommi18@gmail.com>
2025-11-13 16:17:52 +0530

    updated version

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './styles.css';
 function App() {
   return (
     <div className="container">
-      <h1>Welcome to My React Project - S105</h1>
+      <h1>Welcome to My React Project</h1>
       <h1>CI/CD and Cloud DevOps Course</h1>
       <p>This is a simple homepage deployed on GitHub Actions using Vite.</p>
       <h1>Done with Git Actions</h1>
